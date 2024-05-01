6fe420fc1b076e87ed66f9ffcc51f53ccd57aa4b
Грудинин Алексей Михайлович <ip-man2013@yandex.ru>
2024-05-02 02:01:05 +0300

    Добавлены сайты для индексации

---
+++ sql/V1_0_1__add_sites.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.site ("name", status_time, status, url)
+VALUES ('Лента.ру', now(), 'INDEXING', 'https://www.lenta.ru'),
+       ('Skillbox', now(), 'INDEXING', 'https://www.skillbox.ru'),
+       ('PlayBack.Ru', now(), 'INDEXING', 'https://www.playback.ru');
